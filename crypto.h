c1e8c08941e0e320e72d61bb38935bc6cf85154f
kartikisharma <sharmazia@ymail.com>
2016-08-17 21:21:53 -0400

    Create crypto.h

---
+++ crypto.h
@@ -0,0 +1,21 @@
+// A cryptography module 
+
+//
+
+
+
+
+
+
+
+
+
+
+// pig_latin(input, output) stores in output the Pig Latin encoding
+//  of input and prints it out.
+// requires: output is large enough to store the result
+// effects: modifies output
+// time: O(n), where n is the length of input
+
+void pig_latin(char *input, char *output);
+
