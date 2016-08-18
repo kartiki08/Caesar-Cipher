7dd3711df0f967a35d231abb6015f1ec91a824d6
kartikisharma <sharmazia@ymail.com>
2016-08-17 21:16:04 -0400

    Update pig_latin.c

--- pig_latin.c
+++ pig_latin.c
@@ -1,16 +1,11 @@
 
-void piglatin(const char * input, char * output) {
-int rest_index = strlen(input);
-reverse_input(input, output);
-otput[rest_index] = 'a';
-output[rest_index + 1] = 'y';
-output[rest_index + 2] = 0;
-}
-
-void reverse_input(const char *input, char * output) {
-int reverse_index = strlen(input) - 1;
-for (int i = 0; input[i] != 0; i++) {
-output[reverse_index] = input[i];
-reverse_index++;
-}
+void piglatin(char *input, char *output) {
+  int len = strlen(input) - 1;
+  output[len + 1] = 'a';
+  output[len + 2] = 'y';
+  output[len + 3] = 0;
+  *(output + len) = *input;
+  for (int i = 1; i < len; i++) {
+    output[i - 1] = input[i];
+  }
 }
