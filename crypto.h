ee561bc6940623bc177a7274a3cb3310a8f7146b
kartikisharma <sharmazia@ymail.com>
2016-08-17 21:40:39 -0400

    Update crypto.h

--- crypto.h
+++ crypto.h
@@ -1,14 +1,21 @@
 // A cryptography module 
 
-//
+// encode(str, key) encodes str using the Caesar Cipher technique
+//  the help of a key between 1 to 25 (inclusive).
 
 void encode(char *str, int key);
 
 
+// decode(str, key) decodes str using the Caesar Cipher technique
+//  the help of a key between 1 to 25 (inclusive).
+
+void decode (char *str, int key);
 
 
+// input_msg(key, msg) is a set of instructions vital for the I/O
+//  program to function.
 
-
+void input_msg(int key, char *msg);
 
 
 // pig_latin(input, output) stores in output the Pig Latin encoding
