f83c120c5ea05cc560382e5e1bd695dee2f84da1
kartikisharma <sharmazia@ymail.com>
2016-08-17 22:09:01 -0400

    Update pig_latin.c

--- pig_latin.c
+++ pig_latin.c
@@ -1,13 +1,15 @@
 #include "crypto.h"
 #include <strings.h>
 
-void piglatin(char *input, char *output) {
-  int len = strlen(input) - 1;
-  output[len + 1] = 'a';
-  output[len + 2] = 'y';
-  output[len + 3] = 0;
-  *(output + len) = *input;
+void pig_latin(const char *input, char *output) {
+  int len = strlen(input);
   for (int i = 1; i < len; i++) {
     output[i - 1] = input[i];
   }
+  output[len - 1] = *input;
+  output[len] = 'a';
+  output[len + 1] = 'y';
+  output[len + 2] = 0;
 }
+
+
